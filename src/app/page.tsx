5ec7e323423798953406c704bacf8bfe8df8974b
Nagendra Kumar <nagendrakumar0032@gmail.com>
2024-04-13 15:46:37 +0530

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "./_components/header";
+import Hero from "./_components/hero";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
